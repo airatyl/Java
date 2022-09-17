af128e87be581e074f04684b4054a34aa2d6406a
Airatyla@mail.ru <Airatyla@mail.ru>
2022-09-17 12:22:49 +0300

    Initial commit

---
+++ src/ru/mirea/kvbo01/bilyaletdinova/task1/number3.java
@@ -0,0 +1,11 @@
+package ru.mirea.kvbo01.bilyaletdinova.task1;
+
+public class number3 {
+    public static void main(String[] args) {
+        int []arr=new int[10];
+        int sum=0;
+        for(int i=0;i<10;i++){
+            sum+=arr[i];
+        }
+    }
+}
